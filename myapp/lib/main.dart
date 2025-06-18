33e998d6a4f6d9cd590d2a8501d108b2eeb5a787
SpectualDomino <willdomine1@outlook.com>
2025-06-17 19:11:12 -0500

    added env to hide keys

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/entry_portal_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env"); 
+
+  final URL = dotenv.get('URL');
+  final ANON_KEY = dotenv.get('ANON_KEY');
+
   await Supabase.initialize(
-    url: 'https://gxcovwasyqjuaevynldc.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4Y292d2FzeXFqdWFldnlubGRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwODQ2NzEsImV4cCI6MjA0NjY2MDY3MX0.Ywuc_FSoLs5iiBC7rVyErxaSXW5Le-HhPTPPCvRgWqI',
+    url: URL,
+    anonKey:ANON_KEY,
   );
   runApp(const MyApp());
 }
